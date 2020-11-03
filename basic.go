a2a4166e49d476fe9e63290ac76e47e4f5cdf838
Chen Bin <chen2guo@foxmail.com>
2020-11-03 09:44:11 +0800

    add type!

--- basic.go
+++ basic.go
@@ -2,8 +2,69 @@ package main
 
 import "fmt"
 
+func variableZeroValue() {
+	var a int
+	var s string
+
+	fmt.Println(a, s)
+	fmt.Printf("%d    %q\n", a, s)
+}
+
+func varibleInitalValue() {
+	var b, c int = 11, 221
+	var s1 string = "abc" +
+		"hahhaha"
+	fmt.Println(b, c, s1)
+
+}
+
+func varibleTypeDedution1() {
+	var b, c = 11, 222
+	var s1 = "abc" +
+		"hahhaha"
+	fmt.Println(b, c, s1)
+
+}
+
+func varibleTypeDedution2() {
+	b, c := 11, 223
+	s1 := "abc" +
+		"hahhaha"
+	fmt.Println(b, c, s1)
+}
+
+func varibleTypeDedution3() {
+	b, c, s1, t1 := 11, 224, "sixsixsxi", true
+	b = 909
+	fmt.Println(b, c, s1, t1)
+}
+
 func main() {
 	fmt.Println("Hello,World!!")
 	fmt.Println("Hello,World!!")
 	fmt.Println("Hello,World!!")
+
+	variableZeroValue()
+
+	varibleInitalValue()
+
+	varibleTypeDedution1()
+
+	varibleTypeDedution2()
+
+	varibleTypeDedution3()
+
+	//	// step1: cd 到项目下
+	//
+	//	// step2: 移除.idea
+	//	$ git rm --cached -r .idea
+	//	rm '.idea/misc.xml'
+	//rm '.idea/modules.xml'
+	//rm '.idea/vcs.xml'
+	//rm '.idea/workspace.xml'
+	//
+	//	// step3:
+	//	git add .
+	//		git commit -am "rm idea"
+
 }
