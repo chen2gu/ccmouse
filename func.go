9645aac6810dae072e9590c1976775c0544d29bb
Chen Bin <chen2guo@foxmail.com>
2020-11-04 10:17:20 +0800

    add func!

--- func.go
+++ func.go
@@ -2,22 +2,10 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"reflect"
+	"runtime"
 )
-
-func enal(a, b int, op string) int {
-	switch op {
-	case "+":
-		return a + b
-	case "-":
-		return a - b
-	case "*":
-		return a * b
-	case "/":
-		return a / b
-	default:
-		panic("unsupported operation: " + op)
-	}
-}
 
 func div(a, b int) (q, r int) {
 	return a / b, a % b
@@ -27,8 +15,64 @@ func div(a, b int) (q, r int) {
 	//return
 }
 
+func enal(a, b int, op string) (int, error) {
+	switch op {
+	case "+":
+		return a + b, nil
+	case "-":
+		return a - b, nil
+	case "*":
+		return a * b, nil
+	case "/":
+		q, _ := div(a, b)
+		return q, nil
+	default:
+		return 0, fmt.Errorf("unsupported operation: %s", op)
+	}
+}
+
+func apply(op func(int, int) int, a, b int) int {
+	p := reflect.ValueOf(op).Pointer()
+	opName := runtime.FuncForPC(p).Name()
+	//opName :=
+	fmt.Printf("calling function %s with args "+
+		"(%d, %d)\n", opName, a, b)
+	return op(a, b)
+
+}
+
+func pow(a, b int) int {
+	return int(math.Pow(float64(a), float64(b)))
+
+}
+
+func sum(number ...int) int {
+	s := 0
+	for i := range number {
+		s += number[i]
+	}
+	return s
+}
+
 func main() {
-	fmt.Println(enal(3, 4, "+"))
+	if result, err := enal(123, 4, "x"); err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println(result)
+	}
+	fmt.Println("------->")
+	fmt.Println(enal(123, 4, "/s"))
+
+	fmt.Println("------->")
 	q, r := div(13, 4)
 	fmt.Println(q, r)
+
+	fmt.Println("------->")
+	fmt.Println(apply(
+		func(a int, b int) int {
+			return int(math.Pow(float64(a), float64(b)))
+		}, 3, 4))
+
+	fmt.Println("------->")
+	fmt.Println(sum(1, 2, 3, 4))
 }
