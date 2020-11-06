2f5bf2905e0e0b402cf8879067541d8c5aa28991
Chen Bin <chen2guo@foxmail.com>
2020-11-06 10:57:28 +0800

    add Slice!

--- slice.go
+++ slice.go
@@ -33,4 +33,23 @@ func main() {
 	fmt.Println(s2)
 	fmt.Println(arr)
 
+	fmt.Println("S2 ------->")
+	fmt.Println(s2)
+	s2 = s2[:5]
+	fmt.Println(s2)
+	s2 = s2[2:]
+	fmt.Println(s2)
+
+	fmt.Println("Extending slice")
+	fmt.Println("arr=", arr)
+	arr[0], arr[2] = 0, 2
+	s1 = arr[2:6]
+	s2 = s1[3:5]
+	fmt.Println("s1=", s1)
+	fmt.Println("s2=", s2)
+	fmt.Printf("s1=%v,len(s1)=%d,cap(s1)=%d\n", s1, len(s1), cap(s1))
+	fmt.Printf("s2=%v,len(s2)=%d,cap(s2)=%d\n", s2, len(s2), cap(s2))
+
+	fmt.Println(s1[:6])
+
 }
