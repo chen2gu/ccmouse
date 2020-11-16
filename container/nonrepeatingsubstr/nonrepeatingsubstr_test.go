0afa789ced1a64b9ed925c20be4353f9aefb04aa
cheng <chen2guo@foxmail.com>
2020-11-16 23:17:09 +0800

    update chen2gu --> chen2guo

--- container/nonrepeatingsubstr/nonrepeatingsubstr_test.go
+++ container/nonrepeatingsubstr/nonrepeatingsubstr_test.go
@@ -30,16 +30,20 @@ func TestSubstr(t *testing.T) {
 
 // go test -bench .
 func BenchmarkSubstr(b *testing.B) {
-	s := "Yes我爱我的家乡!"
+	s := "黑化肥挥发发灰会花飞灰化肥挥发发黑会飞花"
 	for i := 0; i < 13; i++ {
 		s = s + s
 	}
-	ans := 6
+	b.Logf("len(s) = %d", len(s))
+	ans := 8
+	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
 		actual := length0fNonRepeatingSubStr(s)
 		if actual != ans {
-			b.Errorf("calcTriangle %d ; got %s; expected %d.", actual, s, ans)
+			b.Errorf("got %d for input %s; "+
+				"expected %d",
+				actual, s, ans)
 		}
 	}
 }
