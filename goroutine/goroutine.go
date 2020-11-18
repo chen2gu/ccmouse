dd557ec8a0068b451495d985b389bf1a232356bb
chen2guo <chen2guo@foxmail.com>
2020-11-18 16:08:30 +0800

    add goroutine.go.

--- goroutine/goroutine.go
+++ goroutine/goroutine.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"time"
 )
 
 func main() {
-	for i := 0; i < 10000; i++ {
+	var a [10]int
+	for i := 0; i < 10; i++ {
 		go func(i int) {
 			for {
-				fmt.Printf("Hello From goroutine %d\n", i)
+				a[i]++
+				//fmt.Printf("Hello From goroutine %d\n", i)
+				//fmt.Printf("Hello From goroutine %d\n", a)
+				//fmt.Println(a)
+				runtime.Gosched()
 			}
 		}(i)
 	}
 	time.Sleep(time.Second)
+	fmt.Println(a)
 }
